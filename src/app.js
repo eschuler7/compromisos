fdc9e20a2a1c191eda3d7a230b8fb439dadcc6dd
Emilio Schuler <eschulergodo7@gmail.com>
2017-07-22 01:13:31 -0500

    Actualizar menu y creacion de formulario de registrar

--- src/app.js
+++ src/app.js
@@ -25,6 +25,9 @@ app.post('/login', function(req, res){
   res.render('partial/home');
 });
 
+app.get('/register', function(req, res){
+  res.render('partial/register');
+});
 // Starting NodeJS Server
 app.listen(app.get('port'), '0.0.0.0', function() {
 	console.log('Node.Js Server iniciado en el puerto ' + app.get('port'));
