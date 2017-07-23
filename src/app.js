3c07b88bc8cc3f236ae81fe2d8e60abddf3701e0
Emilio Schuler <eschulergodo7@gmail.com>
2017-07-23 12:11:40 -0500

    Creación de actualizar, consultar y carga masiva de compromisos. Creación de dashboard

--- src/app.js
+++ src/app.js
@@ -21,6 +21,10 @@ app.get('/', function(req, res){
   res.render('login');
 });
 
+app.get('/dashboard', function(req, res){
+  res.render('partial/dashboard');
+});
+
 app.post('/login', function(req, res){
   res.render('partial/home');
 });
@@ -28,6 +32,19 @@ app.post('/login', function(req, res){
 app.get('/register', function(req, res){
   res.render('partial/register');
 });
+
+app.get('/update', function(req, res){
+  res.render('partial/update');
+});
+
+app.get('/select', function(req, res){
+  res.render('partial/select');
+});
+
+app.get('/massive', function(req, res){
+  res.render('partial/massive');
+});
+
 // Starting NodeJS Server
 app.listen(app.get('port'), '0.0.0.0', function() {
 	console.log('Node.Js Server iniciado en el puerto ' + app.get('port'));
