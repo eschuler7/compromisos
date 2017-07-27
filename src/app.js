6d97e30873af8bf3544fccb7a09cdedec96c736f
Joan Delgado <jdelgado@pe.ibm.com>
2017-07-27 14:38:46 -0500

    correcciones

--- src/app.js
+++ src/app.js
@@ -61,5 +61,4 @@ app.use('/secure', secure);
 // Starting NodeJS Server
 app.listen(app.get('port'), '0.0.0.0', function() {
 	console.log('Node.Js Server iniciado en el puerto ' + app.get('port'));
-	console.log(process);
 });
